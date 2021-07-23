22219f7675bc5dd9ecc559053dbe23300a1c0b13
Nikhil-Matcha <nikhil.matcha021@msitprogram.net>
2021-07-23 19:03:46 +0530

    v-2.1

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-data = {}
+var productContainer = document.getElementById('product-info');
+var btn = document.getElementById("btn");
 
-function addItem() {
+btn.addEventListener("click", function () {
 	// body...
-	const f = document.getElementById('itemData');
-	data["name"] = f.elements['name'].value;
+	var ourRequest = new XMLHttpRequest();
+	ourRequest.open('GET', 'data.json');
+	ourRequest.onload = function () {
+		// body...
+		var ourData = JSON.parse(ourRequest.responseText);
+		renderHTML(ourData);
+	};
+	ourRequest.send();
+});
+
+function renderHTML(data){
+	productContainer.insertAdjacentHTML('beforeend', 'testing123');
+	
 }
 
-console.log(data)
+// function addItem() {
+// 	// body...
+// 	var newItem = {};
+// 	const f = document.getElementById('itemData');
+// 	newItem["name"] = f.elements['name'].value;
+// 	newItem["description"] = f.elements['desc'].value;
+// 	newItem["price"] = f.elements['price'].value;
+// 	newItem["manufactured"] = f.elements['mfd'].value;
+// 	newItem["expiry"] = f.elements['exd'].value;
+// 	data.push(newItem);
+// 	console.log(data)
+// }
